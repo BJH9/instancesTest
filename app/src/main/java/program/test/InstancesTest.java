4dc97ab51c402a9a17a6730caa53751096ce2354
BJH9 <tfzangin9@gmail.com>
2021-08-25 18:00:52 +0900

    c

--- app/src/main/java/program/test/InstancesTest.java
+++ app/src/main/java/program/test/InstancesTest.java
@@ -5,10 +5,23 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import weka.core.Instances;
+import weka.core.Instance;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import java.util.Arrays;
 
 public class InstancesTest {
-
-	private String path;
+	private Instances ins;
+	private Instance in;
+	
+	private int column;
+	private int row;
+	
+	private int[][] v;
+	double int[] b;
 	
 	public static void main(String[] args) {
 		InstancesTest runner = new InstancesTest();
@@ -29,14 +42,57 @@ public class InstancesTest {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		instances.setClassIndex(instances.numAttributes()-1);
 		System.out.println(instances);
 		return instances;
 	}
 	
 	private void run(String[] args) {
-	loadArff(args[0]);
-	
-	
+		
+		ins = loadArff(args[0]);
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		in = ins.get(0);
+		column = in.numAttributes();
+		row = ins.size();
+		System.out.println(row);
+		
+		v = new int[column][row];
+		
+		for(int i = 0; i < column-1; i++) {
+			System.out.println("");
+			for(int j = 0; j < row-1; j++) {
+				in = ins.get(j);
+				v[i][j] = (int)in.valueSparse(i);
+				
+					System.out.print(v[i][j] + ",");
+					
+				
+		}
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		for(int i = 0; i < row; i++) {
+			System.out.println("");
+		for(int j = 0; j < column; j++) {
+			Arrays.sort(v[i][j]);
+			System.out.print(v[i][j] +",");
+		}
+		
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		
+		System.out.println(v);
+		
 	}
 
 }
